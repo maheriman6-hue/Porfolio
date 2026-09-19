820a9672a8e64ea6443934e2c1b369e21106a896
Maher Iman <maheriman6@gmail.com>
2026-09-19 17:12:24 +0500

    footer edit

--- src/App.jsx
+++ src/App.jsx
@@ -944,7 +944,7 @@ function Footer() {
       </div>
 
       <div className="footer-wordmark">
-        Iman<span>.</span>
+        Muhammad Iman<span>.</span>
       </div>
 
       <div className="footer-bottom">
